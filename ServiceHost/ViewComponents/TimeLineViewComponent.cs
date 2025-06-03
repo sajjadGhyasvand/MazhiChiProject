220190efdc1ce9a33c05c2551afc2256f53e3e8d
SajadGh <Sajjadgh1995@hotmail.com>
2025-06-03 17:00:59 +0330

    .

---
+++ ServiceHost/ViewComponents/TimeLineViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using My_ShopQuery.Contract.AboutUs;
+using My_ShopQuery.Contract.GeneralSetting;
+
+namespace ServiceHost.ViewComponents
+{
+    public class TimeLineViewComponent : ViewComponent
+    {
+    
+
+        public IViewComponentResult Invoke()
+        {
+            return View("TimeLine");
+        }
+    }
+}
